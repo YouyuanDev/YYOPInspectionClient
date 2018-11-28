b2025937fd1c4638d8084871d25d213bfc811a9c
ketewang <ketewang@youyuantech.com>
2018-11-28 12:43:41 +0800

    修改字体

--- YYOPInspectionClient/MainWindow.Designer.cs
+++ YYOPInspectionClient/MainWindow.Designer.cs
@@ -106,6 +106,7 @@
             // 
             // label1
             // 
+            this.label1.Font = new System.Drawing.Font("黑体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.label1.Location = new System.Drawing.Point(8, 39);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(103, 15);
@@ -114,15 +115,17 @@
             // 
             // label2
             // 
+            this.label2.Font = new System.Drawing.Font("黑体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.label2.Location = new System.Drawing.Point(8, 81);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(62, 16);
+            this.label2.Size = new System.Drawing.Size(72, 16);
             this.label2.TabIndex = 15;
             this.label2.Text = "User Name";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("黑体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.label7.Location = new System.Drawing.Point(9, 97);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(41, 12);
@@ -142,6 +145,7 @@
             // label8
             // 
             this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("黑体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.label8.Location = new System.Drawing.Point(285, 97);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(29, 12);
@@ -198,6 +202,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("黑体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.label6.Location = new System.Drawing.Point(283, 57);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(53, 12);
@@ -207,6 +212,7 @@
             // label10
             // 
             this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("黑体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.label10.Location = new System.Drawing.Point(517, 37);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(29, 12);
@@ -216,6 +222,7 @@
             // 
             // label3
             // 
+            this.label3.Font = new System.Drawing.Font("黑体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.label3.Location = new System.Drawing.Point(283, 81);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(63, 16);
@@ -225,6 +232,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("黑体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.label5.Location = new System.Drawing.Point(8, 57);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(77, 12);
@@ -309,11 +317,14 @@
             // 
             // btnLogin
             // 
+            this.btnLogin.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.btnLogin.Font = new System.Drawing.Font("黑体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.btnLogin.Location = new System.Drawing.Point(498, 52);
             this.btnLogin.Name = "btnLogin";
             this.btnLogin.Size = new System.Drawing.Size(78, 50);
             this.btnLogin.TabIndex = 19;
             this.btnLogin.Text = "登录";
+            this.btnLogin.UseVisualStyleBackColor = false;
             this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
             // 
             // groupBox3
@@ -341,6 +352,7 @@
             // 
             // label4
             // 
+            this.label4.Font = new System.Drawing.Font("黑体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.label4.Location = new System.Drawing.Point(283, 39);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(79, 17);
@@ -349,27 +361,32 @@
             // 
             // btnHide
             // 
-            this.btnHide.Location = new System.Drawing.Point(645, 30);
+            this.btnHide.BackColor = System.Drawing.Color.DarkRed;
+            this.btnHide.Font = new System.Drawing.Font("黑体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnHide.Location = new System.Drawing.Point(645, 47);
             this.btnHide.Name = "btnHide";
-            this.btnHide.Size = new System.Drawing.Size(126, 47);
+            this.btnHide.Size = new System.Drawing.Size(126, 79);
             this.btnHide.TabIndex = 77;
             this.btnHide.Text = "关闭";
-            this.btnHide.UseVisualStyleBackColor = true;
+            this.btnHide.UseVisualStyleBackColor = false;
             this.btnHide.Click += new System.EventHandler(this.btnHide_Click);
             // 
             // btnPreview
             // 
+            this.btnPreview.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.btnPreview.Font = new System.Drawing.Font("黑体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.btnPreview.Location = new System.Drawing.Point(6, 210);
             this.btnPreview.Name = "btnPreview";
             this.btnPreview.Size = new System.Drawing.Size(139, 125);
             this.btnPreview.TabIndex = 64;
             this.btnPreview.Text = "启动录像机";
-            this.btnPreview.UseVisualStyleBackColor = true;
+            this.btnPreview.UseVisualStyleBackColor = false;
             this.btnPreview.Click += new System.EventHandler(this.btnPreview_Click_1);
             // 
             // label13
             // 
             this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("黑体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.label13.Location = new System.Drawing.Point(86, 127);
             this.label13.Name = "label13";
             this.label13.Size = new System.Drawing.Size(77, 12);
@@ -379,6 +396,7 @@
             // 
             // comboBoxView
             // 
+            this.comboBoxView.Font = new System.Drawing.Font("黑体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.comboBoxView.FormattingEnabled = true;
             this.comboBoxView.Items.AddRange(new object[] {
             "Direct:直接预览 ",
@@ -390,6 +408,7 @@
             // 
             // btnRefresh
             // 
+            this.btnRefresh.Font = new System.Drawing.Font("黑体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.btnRefresh.Location = new System.Drawing.Point(6, 122);
             this.btnRefresh.Name = "btnRefresh";
             this.btnRefresh.Size = new System.Drawing.Size(74, 36);
@@ -403,6 +422,7 @@
             this.listViewIPChannel.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.ColumnHeader1,
             this.ColumnHeader2});
+            this.listViewIPChannel.Font = new System.Drawing.Font("黑体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.listViewIPChannel.FullRowSelect = true;
             this.listViewIPChannel.GridLines = true;
             this.listViewIPChannel.Location = new System.Drawing.Point(6, 20);
--- YYOPInspectionClient/MessagePrompt.Designer.cs
+++ YYOPInspectionClient/MessagePrompt.Designer.cs
@@ -83,7 +83,7 @@
             // 
             // label2
             // 
-            this.label2.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Font = new System.Drawing.Font("黑体", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.label2.Location = new System.Drawing.Point(7, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(552, 112);
